1b3fdcb8e1a90ea00b0d93bcb405891b64acf3ec
Leonch03 <128586811+Leonch03@users.noreply.github.com>
2023-08-22 09:49:10 +0200

    Update vincoli.sql

--- vincoli.sql
+++ vincoli.sql
@@ -1,4 +1,29 @@
 -- Artista(Attore) e Artista(Regista) non possono essere entrambi nulli;
 DROP TRIGGER IF EXISTS artista_o_regista;
+
+DELIMITER $$
+
 CREATE TRIGGER artista_o_regista
-BEFORE INSERT ON Artista
+BEFORE INSERT ON Artista FOR EACH ROW
+IF NEW.Attore == NULL AND NEW.Regista == NULL THEN
+  SIGNAL SQLSTATE '45000'
+  SET MESSAGE_TEXT = 'Attore e Regista non possono essere entrambi nulli'
+END IF
+
+END $$
+DELIMITER;
+
+-- La data definita dagli attributi CartaDiCredito(Mese) e CartaDiCredito(Anno) deve essere futura
+DROP TRIGGER IF EXISTS data_carta_future;
+
+DELIMITER $$
+
+CREATE TRIGGER data_carta_futura
+BEFORE INSERT ON CartaDiCredito FOR EACH ROW
+IF NEW.Attore == NULL AND NEW.Regista == NULL THEN
+  SIGNAL SQLSTATE '45000'
+  SET MESSAGE_TEXT = 'Attore e Regista non possono essere entrambi nulli'
+END IF
+
+END $$
+DELIMITER;
