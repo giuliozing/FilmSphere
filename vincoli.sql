df6486c20b92aeea86633c9dca547d79982ea617
Leonch03 <128586811+Leonch03@users.noreply.github.com>
2023-08-22 09:41:37 +0200

    Update vincoli.sql

--- vincoli.sql
+++ vincoli.sql
@@ -1,1 +1,4 @@
-DROP TRIGGER IF EXISTS
+-- Artista(Attore) e Artista(Regista) non possono essere entrambi nulli;
+DROP TRIGGER IF EXISTS artista_o_regista;
+CREATE TRIGGER artista_o_regista
+BEFORE INSERT ON Artista
