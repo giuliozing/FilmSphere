24afb0c849ca987405da83f3ad26636b8c7399f2
Leonch03 <128586811+Leonch03@users.noreply.github.com>
2023-08-22 09:54:26 +0200

    Update vincoli.sql

--- vincoli.sql
+++ vincoli.sql
@@ -14,15 +14,15 @@ END $$
 DELIMITER;
 
 -- La data definita dagli attributi CartaDiCredito(Mese) e CartaDiCredito(Anno) deve essere futura
-DROP TRIGGER IF EXISTS data_carta_future;
+DROP TRIGGER IF EXISTS data_carta_futura;
 
 DELIMITER $$
 
 CREATE TRIGGER data_carta_futura
 BEFORE INSERT ON CartaDiCredito FOR EACH ROW
-IF NEW.Attore == NULL AND NEW.Regista == NULL THEN
+IF NEW.Anno < YEAR(CURRENT_DATE) OR (NEW.Anno == YEAR(CURRENT_DATE) AND NEW.Mese <= MONTH(CURRENT_DATE))
   SIGNAL SQLSTATE '45000'
-  SET MESSAGE_TEXT = 'Attore e Regista non possono essere entrambi nulli'
+  SET MESSAGE_TEXT = 'Carta Scaduta'
 END IF
 
 END $$
