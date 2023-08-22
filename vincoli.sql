f3fe6cfc723f4f35996e5c88b0fafb6751947c7f
Leonch03 <128586811+Leonch03@users.noreply.github.com>
2023-08-22 10:42:50 +0200

    Update vincoli.sql

--- vincoli.sql
+++ vincoli.sql
@@ -1,3 +1,4 @@
+-- Vincoli intrarelazionali di n-upla
 -- Artista(Attore) e Artista(Regista) non possono essere entrambi nulli;
 DROP TRIGGER IF EXISTS artista_o_regista;
 
@@ -5,10 +6,11 @@ DELIMITER $$
 
 CREATE TRIGGER artista_o_regista
 BEFORE INSERT ON Artista FOR EACH ROW
-IF NEW.Attore == NULL AND NEW.Regista == NULL THEN
-  SIGNAL SQLSTATE '45000'
-  SET MESSAGE_TEXT = 'Attore e Regista non possono essere entrambi nulli'
-END IF
+BEGIN
+  IF NEW.Attore == NULL AND NEW.Regista == NULL THEN
+    SIGNAL SQLSTATE '45000'
+    SET MESSAGE_TEXT = 'Attore e Regista non possono essere entrambi nulli'
+  END IF
 
 END $$
 DELIMITER;
@@ -20,10 +22,11 @@ DELIMITER $$
 
 CREATE TRIGGER data_carta_futura
 BEFORE INSERT ON CartaDiCredito FOR EACH ROW
-IF NEW.Anno < YEAR(CURRENT_DATE) OR (NEW.Anno == YEAR(CURRENT_DATE) AND NEW.Mese <= MONTH(CURRENT_DATE)) THEN
-  SIGNAL SQLSTATE '45000'
-  SET MESSAGE_TEXT = 'Carta Scaduta'
-END IF
+BEGIN
+  IF NEW.Anno < YEAR(CURRENT_DATE) OR (NEW.Anno == YEAR(CURRENT_DATE) AND NEW.Mese <= MONTH(CURRENT_DATE)) THEN
+    SIGNAL SQLSTATE '45000'
+    SET MESSAGE_TEXT = 'Carta Scaduta'
+  END IF
 
 END $$
 DELIMITER;
@@ -35,10 +38,65 @@ DELIMITER $$
 
 CREATE TRIGGER ip_paese
 BEFORE INSERT ON Paese FOR EACH ROW
-IF NEW.InizioIP >= NEW.FineIP THEN
-  SIGNAL SQLSTATE '45000'
-  SET MESSAGE_TEXT = 'Paese(InizioIP) deve essere minore di Paese(FineIP)'
-END IF
+BEGIN
+  IF NEW.InizioIP >= NEW.FineIP THEN
+    SIGNAL SQLSTATE '45000'
+    SET MESSAGE_TEXT = 'Paese(InizioIP) deve essere minore di Paese(FineIP)'
+  END IF
 
 END $$
 DELIMITER;
+
+--Premio(Attore), Premio(Film), Premio(Regista) non possono essere tutti nulli
+DROP TRIGGER IF EXISTS premio;
+
+DELIMITER $$
+
+CREATE TRIGGER premio
+BEFORE INSERT ON Premio FOR EACH ROW
+BEGIN
+  IF NEW.Attore == NULL AND NEW.Film == NULL AND NEW.Regista == NULL THEN
+    SIGNAL SQLSTATE '45000'
+    SET MESSAGE_TEXT = 'Premio(Attore), Premio(Film), Premio(Regista) non possono essere tutti nulli'
+  END IF
+
+END $$
+DELIMITER;
+
+--Altri vincoli interrelazionali
+--Un Artista può comparire nella tabella Interpretazione solo se il suo attributo Attore vale 1
+DROP TRIGGER IF EXISTS interpretazione_artista_1;
+
+DELIMITER $$
+
+CREATE TRIGGER interpretazione_artista_1
+BEFORE INSERT ON Interpretazione FOR EACH ROW
+BEGIN
+  DECLARE artista INT DEFAULT 0;
+  SET artista = 
+    (SELECT A.Attore
+     FROM Artista A
+     WHERE A.Id = NEW.Artista
+    );
+  IF artista == 0 THEN
+    SIGNAL SQLSTATE '45000'
+    SET MESSAGE_TEXT = 'Un Artista può comparire nella tabella Interpretazione solo se il suo attributo Attore vale 1'
+  END IF
+
+END $$
+DELIMITER;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
