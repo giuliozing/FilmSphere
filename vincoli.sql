1cb0f4ed4b970a180e69b50b59135f5632b62475
Leonch03 <128586811+Leonch03@users.noreply.github.com>
2023-08-25 16:08:56 +0200

    Update vincoli.sql

--- vincoli.sql
+++ vincoli.sql
@@ -10,11 +10,11 @@ BEFORE INSERT ON CartaDiCredito FOR EACH ROW
 BEGIN
   IF NEW.Numero < 1000000000000000 OR NEW.Numero > 9999999999999999 THEN
     SIGNAL SQLSTATE '45000'
-    SET MESSAGE_TEXT = 'Numero Carta di Credito non valido'
-  END IF
+    SET MESSAGE_TEXT = 'Numero Carta di Credito non valido';
+  END IF;
 
-END 
-DELIMITER $$;
+END $$
+DELIMITER ;
 
 -- CVV Carta di Credito
 DROP TRIGGER IF EXISTS cvv_carta;
@@ -26,11 +26,11 @@ BEFORE INSERT ON CartaDiCredito FOR EACH ROW
 BEGIN
   IF NEW.CVV < 100 OR NEW.CVV > 999 THEN
     SIGNAL SQLSTATE '45000'
-    SET MESSAGE_TEXT = 'CVV Carta di Credito non valido'
-  END IF
+    SET MESSAGE_TEXT = 'CVV Carta di Credito non valido';
+  END IF;
 
-END 
-DELIMITER $$;
+END $$
+DELIMITER ;
 
 -- Mese di Scadenza Carta di Credito
 DROP TRIGGER IF EXISTS mesescadenza_carta;
@@ -42,11 +42,11 @@ BEFORE INSERT ON CartaDiCredito FOR EACH ROW
 BEGIN
   IF NEW.MeseScadenza < 1 OR NEW.MeseScadenza > 12 THEN
     SIGNAL SQLSTATE '45000'
-    SET MESSAGE_TEXT = 'Mese di Scadenza Carta di Credito non valido'
-  END IF
+    SET MESSAGE_TEXT = 'Mese di Scadenza Carta di Credito non valido';
+  END IF;
 
-END 
-DELIMITER $$;
+END $$
+DELIMITER ;
 
 -- Anno di Scadenza Carta di Credito
 DROP TRIGGER IF EXISTS annoscadenza_carta;
@@ -58,11 +58,11 @@ BEFORE INSERT ON CartaDiCredito FOR EACH ROW
 BEGIN
   IF NEW.AnnoScadenza < 2023 THEN
     SIGNAL SQLSTATE '45000'
-    SET MESSAGE_TEXT = 'Mese di Scadenza Carta di Credito non valido'
-  END IF
+    SET MESSAGE_TEXT = 'Mese di Scadenza Carta di Credito non valido';
+  END IF;
 
-END 
-DELIMITER $$;
+END $$
+DELIMITER ;
 
 -- IP Connessione
 DROP TRIGGER IF EXISTS ip_connessione;
@@ -74,11 +74,11 @@ BEFORE INSERT ON Connessione FOR EACH ROW
 BEGIN
   IF NEW.IP < 0 OR NEW.IP > 255255255255 THEN
     SIGNAL SQLSTATE '45000'
-    SET MESSAGE_TEXT = 'IP non valido'
-  END IF
+    SET MESSAGE_TEXT = 'IP non valido';
+  END IF;
 
-END 
-DELIMITER $$;
+END $$
+DELIMITER ;
 
 -- Carta di Credito della Fattura
 DROP TRIGGER IF EXISTS carta_fattura;
@@ -90,11 +90,11 @@ BEFORE INSERT ON Fattura FOR EACH ROW
 BEGIN
   IF NEW.CartaDiCredito < 1000000000000000 OR NEW.CartaDiCredito > 9999999999999999 THEN
     SIGNAL SQLSTATE '45000'
-    SET MESSAGE_TEXT = 'Numero Carta di Credito non valido'
-  END IF
+    SET MESSAGE_TEXT = 'Numero Carta di Credito non valido';
+  END IF;
 
-END 
-DELIMITER $$;
+END $$
+DELIMITER ;
 
 -- Anno Film
 DROP TRIGGER IF EXISTS anno_film;
@@ -106,11 +106,11 @@ BEFORE INSERT ON Film FOR EACH ROW
 BEGIN
   IF NEW.Anno < 1900 OR NEW.Anno > YEAR(CURRENT_DATE) THEN
     SIGNAL SQLSTATE '45000'
-    SET MESSAGE_TEXT = 'Anno non valido'
-  END IF
+    SET MESSAGE_TEXT = 'Anno non valido';
+  END IF;
 
-END 
-DELIMITER $$;
+END $$
+DELIMITER ;
 
 -- Valore di importanza
 DROP TRIGGER IF EXISTS valore_importanza;
@@ -122,11 +122,11 @@ BEFORE INSERT ON Importanza FOR EACH ROW
 BEGIN
   IF NEW.Valore < 1 OR NEW.Valore > 100 THEN
     SIGNAL SQLSTATE '45000'
-    SET MESSAGE_TEXT = 'Valore di importanza non valido'
-  END IF
+    SET MESSAGE_TEXT = 'Valore di importanza non valido';
+  END IF;
 
-END 
-DELIMITER $$;
+END $$
+DELIMITER ;
 
 -- Inizio IP Paese
 DROP TRIGGER IF EXISTS inizioip_paese;
@@ -136,13 +136,13 @@ DELIMITER $$
 CREATE TRIGGER inizioip_paese
 BEFORE INSERT ON Paese FOR EACH ROW
 BEGIN
-  IF NEW.InzioIP < 0 OR NEW.InizioIP > 255255255255 THEN
+  IF NEW.InizioIP < 0 OR NEW.InizioIP > 255255255255 THEN
     SIGNAL SQLSTATE '45000'
-    SET MESSAGE_TEXT = 'IP non valido'
-  END IF
+    SET MESSAGE_TEXT = 'IP non valido';
+  END IF;
 
-END 
-DELIMITER $$;
+END $$
+DELIMITER ;
 
 -- Fine IP Paese
 DROP TRIGGER IF EXISTS fineip_paese;
@@ -154,11 +154,11 @@ BEFORE INSERT ON Paese FOR EACH ROW
 BEGIN
   IF NEW.FineIP < 0 OR NEW.FineIP > 255255255255 THEN
     SIGNAL SQLSTATE '45000'
-    SET MESSAGE_TEXT = 'IP non valido'
-  END IF
+    SET MESSAGE_TEXT = 'IP non valido';
+  END IF;
 
-END 
-DELIMITER $$;
+END $$
+DELIMITER ;
 
 -- Peso del Premio
 DROP TRIGGER IF EXISTS peso_premio;
@@ -170,11 +170,11 @@ BEFORE INSERT ON Premio FOR EACH ROW
 BEGIN
   IF NEW.Peso < 0 OR NEW.Peso > 100 THEN
     SIGNAL SQLSTATE '45000'
-    SET MESSAGE_TEXT = 'Peso non valido'
-  END IF
+    SET MESSAGE_TEXT = 'Peso non valido';
+  END IF;
 
-END 
-DELIMITER $$;
+END $$
+DELIMITER ;
 
 -- Voto recensione del critico
 DROP TRIGGER IF EXISTS voto_recensionecritico;
@@ -186,11 +186,11 @@ BEFORE INSERT ON RecensioneCritico FOR EACH ROW
 BEGIN
   IF NEW.Voto < 1 OR NEW.Voto > 5 THEN
     SIGNAL SQLSTATE '45000'
-    SET MESSAGE_TEXT = 'Voto non valido'
-  END IF
+    SET MESSAGE_TEXT = 'Voto non valido';
+  END IF;
 
-END 
-DELIMITER $$;
+END $$
+DELIMITER ;
 
 -- Voto recensione dell'utente
 DROP TRIGGER IF EXISTS voto_recensioneutente;
@@ -202,11 +202,11 @@ BEFORE INSERT ON RecensioneUtente FOR EACH ROW
 BEGIN
   IF NEW.Voto < 1 OR NEW.Voto > 5 THEN
     SIGNAL SQLSTATE '45000'
-    SET MESSAGE_TEXT = 'Voto non valido'
-  END IF
+    SET MESSAGE_TEXT = 'Voto non valido';
+  END IF;
 
-END 
-DELIMITER $$;
+END $$
+DELIMITER ;
 
 -- Jitter del Server
 DROP TRIGGER IF EXISTS jitter_server;
@@ -218,11 +218,11 @@ BEFORE INSERT ON Server FOR EACH ROW
 BEGIN
   IF NEW.Jitter < 1 OR NEW.Jitter > 10 THEN
     SIGNAL SQLSTATE '45000'
-    SET MESSAGE_TEXT = 'Jitter non valido'
-  END IF
+    SET MESSAGE_TEXT = 'Jitter non valido';
+  END IF;
 
-END 
-DELIMITER $$;
+END $$
+DELIMITER ;
 
 -- Carta di Credito dell'Utente
 DROP TRIGGER IF EXISTS carta_utente;
@@ -234,11 +234,11 @@ BEFORE INSERT ON Utente FOR EACH ROW
 BEGIN
   IF NEW.CartaDiCredito < 1000000000000000 OR NEW.CartaDiCredito > 9999999999999999 THEN
     SIGNAL SQLSTATE '45000'
-    SET MESSAGE_TEXT = 'Numero Carta di Credito non valido'
-  END IF
+    SET MESSAGE_TEXT = 'Numero Carta di Credito non valido';
+  END IF;
 
-END 
-DELIMITER $$;
+END $$
+DELIMITER ;
 
 -- Vincoli intrarelazionali di n-upla
 -- Artista(Attore) e Artista(Regista) non possono essere entrambi nulli;
@@ -249,13 +249,13 @@ DELIMITER $$
 CREATE TRIGGER artista_o_regista
 BEFORE INSERT ON Artista FOR EACH ROW
 BEGIN
-  IF NEW.Attore == NULL AND NEW.Regista == NULL THEN
+  IF NEW.Attore = NULL AND NEW.Regista = NULL THEN
     SIGNAL SQLSTATE '45000'
-    SET MESSAGE_TEXT = 'Attore e Regista non possono essere entrambi nulli'
-  END IF
+    SET MESSAGE_TEXT = 'Attore e Regista non possono essere entrambi nulli';
+  END IF;
 
-END 
-DELIMITER $$;
+END $$
+DELIMITER ;
 
 -- La data definita dagli attributi CartaDiCredito(Mese) e CartaDiCredito(Anno) deve essere futura
 DROP TRIGGER IF EXISTS data_carta_futura;
@@ -265,13 +265,13 @@ DELIMITER $$
 CREATE TRIGGER data_carta_futura
 BEFORE INSERT ON CartaDiCredito FOR EACH ROW
 BEGIN
-  IF NEW.Anno < YEAR(CURRENT_DATE) OR (NEW.Anno == YEAR(CURRENT_DATE) AND NEW.Mese <= MONTH(CURRENT_DATE)) THEN
+  IF NEW.AnnoScadenza < YEAR(CURRENT_DATE) OR (NEW.AnnoScadenza = YEAR(CURRENT_DATE) AND NEW.MeseScadenza <= MONTH(CURRENT_DATE)) THEN
     SIGNAL SQLSTATE '45000'
-    SET MESSAGE_TEXT = 'Carta Scaduta'
-  END IF
+    SET MESSAGE_TEXT = 'Carta Scaduta';
+  END IF;
 
-END 
-DELIMITER $$;
+END $$
+DELIMITER ;
 
 -- Paese(InizioIP) deve essere minore di Paese(FineIP)
 DROP TRIGGER IF EXISTS ip_paese;
@@ -283,11 +283,11 @@ BEFORE INSERT ON Paese FOR EACH ROW
 BEGIN
   IF NEW.InizioIP >= NEW.FineIP THEN
     SIGNAL SQLSTATE '45000'
-    SET MESSAGE_TEXT = 'Paese(InizioIP) deve essere minore di Paese(FineIP)'
-  END IF
+    SET MESSAGE_TEXT = 'Paese(InizioIP) deve essere minore di Paese(FineIP)';
+  END IF;
 
-END 
-DELIMITER $$;
+END $$
+DELIMITER ;
 
 -- Premio(Attore), Premio(Film), Premio(Regista) non possono essere tutti nulli
 DROP TRIGGER IF EXISTS premio;
@@ -297,13 +297,13 @@ DELIMITER $$
 CREATE TRIGGER premio
 BEFORE INSERT ON Premio FOR EACH ROW
 BEGIN
-  IF NEW.Attore == NULL AND NEW.Film == NULL AND NEW.Regista == NULL THEN
+  IF NEW.Attore = NULL AND NEW.Film = NULL AND NEW.Regista = NULL THEN
     SIGNAL SQLSTATE '45000'
-    SET MESSAGE_TEXT = 'Premio(Attore), Premio(Film), Premio(Regista) non possono essere tutti nulli'
-  END IF
+    SET MESSAGE_TEXT = 'Premio(Attore), Premio(Film), Premio(Regista) non possono essere tutti nulli';
+  END IF;
 
-END 
-DELIMITER $$;
+END $$
+DELIMITER ;
 
 -- Altri vincoli interrelazionali
 -- Un Artista può comparire nella tabella Interpretazione solo se il suo attributo Attore vale 1
@@ -320,13 +320,13 @@ BEGIN
      FROM Artista A
      WHERE A.Id = NEW.Artista
     );
-  IF artista == 0 THEN
+  IF artista = 0 THEN
     SIGNAL SQLSTATE '45000'
-    SET MESSAGE_TEXT = 'Un Artista può comparire nella tabella Interpretazione solo se il suo attributo Attore vale 1'
-  END IF
+    SET MESSAGE_TEXT = 'Un Artista può comparire nella tabella Interpretazione solo se il suo attributo Attore vale 1';
+  END IF;
 
-END 
-DELIMITER $$;
+END $$
+DELIMITER ;
 
 -- Un Artista può comparire nella tabella Direzione solo se il suo attributo Regista vale 1
 DROP TRIGGER IF EXISTS direzione_regista_1;
@@ -342,13 +342,13 @@ BEGIN
      FROM Artista A
      WHERE A.Id = NEW.Artista
     );
-  IF artista == 0 THEN
+  IF artista = 0 THEN
     SIGNAL SQLSTATE '45000'
-    SET MESSAGE_TEXT = 'Un Artista può comparire nella tabella Direzione solo se il suo attributo Regista vale 1'
-  END IF
+    SET MESSAGE_TEXT = 'Un Artista può comparire nella tabella Direzione solo se il suo attributo Regista vale 1';
+  END IF;
 
-END 
-DELIMITER $$;
+END $$
+DELIMITER ;
 
 -- Un Artista può comparire nella tabella PremiazioneAttore solo se il suo attributo Attore vale 1
 DROP TRIGGER IF EXISTS premiazione_attore_attore_1;
@@ -362,15 +362,15 @@ BEGIN
   SET artista = 
     (SELECT A.Attore
      FROM Artista A
-     WHERE A.Id = NEW.Artista
+     WHERE A.Id = NEW.Attore
     );
-  IF artista == 0 THEN
+  IF artista = 0 THEN
     SIGNAL SQLSTATE '45000'
-    SET MESSAGE_TEXT = 'Un Artista può comparire nella tabella PremiazioneAttore solo se il suo attributo Attore vale 1'
-  END IF
+    SET MESSAGE_TEXT = 'Un Artista può comparire nella tabella PremiazioneAttore solo se il suo attributo Attore vale 1';
+  END IF;
 
-END 
-DELIMITER $$;
+END $$
+DELIMITER ;
 
 -- Un Premio può comparire nella tabella PremiazioneAttore solo se il suo attributo Attore vale 1
 DROP TRIGGER IF EXISTS premiazione_attore_premio_1;
@@ -386,13 +386,13 @@ BEGIN
      FROM Premio P
      WHERE P.Id = NEW.Premio
     );
-  IF premio == 0 THEN
+  IF premio = 0 THEN
     SIGNAL SQLSTATE '45000'
-    SET MESSAGE_TEXT = 'Un Premio può comparire nella tabella PremiazioneAttore solo se il suo attributo Attore vale 1'
-  END IF
+    SET MESSAGE_TEXT = 'Un Premio può comparire nella tabella PremiazioneAttore solo se il suo attributo Attore vale 1';
+  END IF;
 
-END 
-DELIMITER $$;
+END $$
+DELIMITER ;
 
 -- Un Premio può comparire nella tabella PremiazioneFilm solo se il suo attributo Film vale 1
 DROP TRIGGER IF EXISTS premiazione_film_1;
@@ -408,13 +408,13 @@ BEGIN
      FROM Premio P
      WHERE P.Id = NEW.Premio
     );
-  IF premio == 0 THEN
+  IF premio = 0 THEN
     SIGNAL SQLSTATE '45000'
-    SET MESSAGE_TEXT = 'Un Premio può comparire nella tabella PremiazioneFilm solo se il suo attributo Film vale 1'
-  END IF
+    SET MESSAGE_TEXT = 'Un Premio può comparire nella tabella PremiazioneFilm solo se il suo attributo Film vale 1';
+  END IF;
 
-END 
-DELIMITER $$;
+END $$
+DELIMITER ;
 
 -- Un Artista può comparire nella tabella PremiazioneRegista solo se il suo attributo Regista vale 1
 DROP TRIGGER IF EXISTS premiazione_regista_regista_1;
@@ -428,15 +428,15 @@ BEGIN
   SET artista = 
     (SELECT A.Regista
      FROM Artista A
-     WHERE A.Id = NEW.Artista
+     WHERE A.Id = NEW.Regista
     );
-  IF artista == 0 THEN
+  IF artista = 0 THEN
     SIGNAL SQLSTATE '45000'
-    SET MESSAGE_TEXT = 'Un Artista può comparire nella tabella PremiazioneRegista solo se il suo attributo Regista vale 1'
-  END IF
+    SET MESSAGE_TEXT = 'Un Artista può comparire nella tabella PremiazioneRegista solo se il suo attributo Regista vale 1';
+  END IF;
 
-END 
-DELIMITER $$;
+END $$
+DELIMITER ;
 
 -- Un Premio può comparire nella tabella PremiazioneRegista solo se il suo attributo Regista vale 1
 DROP TRIGGER IF EXISTS premiazione_regista_premio_1;
@@ -452,13 +452,13 @@ BEGIN
      FROM Premio P
      WHERE P.Id = NEW.Premio
     );
-  IF premio == 0 THEN
+  IF premio = 0 THEN
     SIGNAL SQLSTATE '45000'
-    SET MESSAGE_TEXT = 'Un Premio può comparire nella tabella PremiazioneRegista solo se il suo attributo Regista vale 1'
-  END IF
+    SET MESSAGE_TEXT = 'Un Premio può comparire nella tabella PremiazioneRegista solo se il suo attributo Regista vale 1';
+  END IF;
 
-END 
-DELIMITER $$;
+END $$
+DELIMITER ;
 
 -- Vincoli di integrità referenziale
 -- Ogni film in Appartenenza deve comparire nella tabella Film
